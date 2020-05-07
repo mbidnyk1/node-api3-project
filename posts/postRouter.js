252cdc479a9499c70778836a4706d771de14d2fb
Michael Bidnyk <mbidnyk1@gmail.com>
2020-05-07 15:28:08 -0500

    code added for user and post endpoints

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,76 @@
 const express = require('express');
 
 const router = express.Router();
+const posts = require('./postDb')
+const validatePost = require('../utils/validatePost')
+
 
 router.get('/', (req, res) => {
   // do your magic!
+  posts.get()
+  .then( dbPosts => {
+    res.status(200).json(dbPosts)
+  })
+  .catch((err) => {
+    console.log({err})
+    res.status(500).json({
+      error: 'The post information could not be retrieved.'
+    })
+  })
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  posts.getById(req.params.id)
+  .then( post => {
+    if (post) {
+    res.status(200).json(post)
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
+  })
+  .catch((err) => {
+    console.log({err})
+    res.status(500).json({ error: "The post information could not be retrieved." })
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  posts.remove(req.params.id)
+  .then(post => {
+    if(post === 1) {
+      res.status(200).json({ message: "Removed post from the database." })
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist."})
+    }
+  })
+  .catch((err) => {
+    console.log({err})
+    res.status(500).json({ error: "The post information could not be deleted." })
+  })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',validatePost, (req, res) => {
   // do your magic!
+  posts.update(req.params.id,req.body)
+  .then(post => {
+    if(post === 1) {
+      res.status(200).json({ message: "Updated post in the database."})
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist." })
+    }
+  })
+  .catch((err) => {
+    console.log({err})
+    .res.status(500).json({ error: "The post information could not be modified." })
+  })
 });
 
 // custom middleware
 
-function validatePostId(req, res, next) {
-  // do your magic!
-}
+// function validatePostId(req, res, next) {
+//   // do your magic!
+// }
 
 module.exports = router;
